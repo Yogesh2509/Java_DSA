404d10ead7a622bd65ccf2f14e30e98c26631c51
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-02-08 15:50:04 +0530

    JAVA BASICS

--- JavaBasics/helloWorld.java
+++ JavaBasics/helloWorld.java
@@ -1,4 +1,4 @@
-package JavaBasics;
+
 class helloWorld{
     public static void main(String[] args) {
         System.out.print("Helllo World");
---
+++ JavaBasics/typeCasting.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class typeCasting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        float a = 2.4f;
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
