9c0e1e861e0a8a1387746c36ffbd3f536d5054af
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-02-11 21:25:19 +0530

    Java Operators

---
+++ operators.java/assignmentOperator.java
@@ -0,0 +1,13 @@
+public class assignmentOperator{
+public static void main(String[] args) {
+    int a = 10;
+    // a = a + 10;
+    a += 10;    // this line is same as the upper commented line ..it is to show the use of assignment operator
+    System.out.println(a);
+
+    int b = 5;
+    //b = b * 5;
+    b *= 5;
+    System.out.println(b);
+}
+}
---
+++ operators.java/logicalOperators.java
@@ -0,0 +1,9 @@
+public class logicalOperators {
+    public static void main(String[] args) {
+        System.out.println((5 > 2) && (9 > 7));   //logical and ,,if both the statements are true than the answer will also be true
+
+        System.out.println((5 > 7 ) || ( 3 > 2));  // logical Or ,,,if any one statement is true than the answer will be true itself
+
+        System.out.println(!(3>2));      //logical NOT ,,,if the answer is true it will reverse it and make it false and vice versa
+    }
+}
