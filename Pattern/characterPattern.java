af1fbbce70f70f664a4c250c34c690e4af7094a6
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-02-22 23:39:47 +0530

    Java codes

--- Pattern/characterPattern.java
+++ Pattern/characterPattern.java
@@ -1,7 +1,7 @@
 public class characterPattern {
     public static void main(String[] args) {
         char ch =  'A';
-        for(int i = 1; i <= 4; i++){
+        for(int i = 1; i <= 6; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print(ch);
                 ch++;
