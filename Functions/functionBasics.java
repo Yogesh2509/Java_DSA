bed240a2680b6698f9ef611159a781c43e588c64
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-02-23 23:47:30 +0530

    Java Codes

---
+++ Functions/functionBasics.java
@@ -0,0 +1,12 @@
+public class functionBasics {
+    public static void printHelloWorld() {
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHelloWorld();  //calling of the function
+                
+    }
+}
